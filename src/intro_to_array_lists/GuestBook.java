e783e3c2c07ab79e06a47583d5870f3a0df1b2aa
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-11-27 18:11:50 -0800

    GuestBook

--- src/intro_to_array_lists/GuestBook.java
+++ src/intro_to_array_lists/GuestBook.java
@@ -1,15 +1,38 @@
 package intro_to_array_lists;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-public class GuestBook {
+public class GuestBook implements ActionListener{
 	// Create a GUI with two buttons. One button reads "Add Name" and the other button reads "View Names". 
 	JFrame frame = new JFrame();
 	JPanel panel = new JPanel();
 	JButton button1 = new JButton();
 	JButton button2 = new JButton();
+	public static void main(String[] args) {
+		GuestBook run = new GuestBook();
+	}
+	GuestBook(){
+	frame.setVisible(true);
+	frame.add(panel);
+	panel.add(button1);
+	panel.add(button2);
+	button1.setText("Add name");
+	button2.setText("View Names");
+	frame.pack();
+		
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		
+		
+	}
+	
 	// When the add name button is clicked, display an input dialog that asks the user to enter a name. Add
 	// that name to an ArrayList. When the "View Names" button is clicked, display a message dialog that displays
 	// all the names added to the list. Format the list as follows:
